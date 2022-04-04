29f2a9afb6947085acfef38a03bc78d080209801
Christopher Delecluse <christopherdeleclusepro@gmail.com>
2022-04-04 22:21:36 +0200

    we are good with data

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,70 +1,3 @@
-// const movie_id = 6038
-//
-// const fetchData = async () => {
-//     const data = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=46077ce24d4f82dbcaa2828a33a34916`)
-//     return await data.json()
-// }
-//
-// const imgPath = 'https://image.tmdb.org/t/p/w500/'
-// let imgFullPath = ''
-//
-// fetchData()
-// .then(r => {
-//     imgFullPath = imgPath + r["poster_path"]
-//     img.src = imgFullPath
-// })
-//
-// const img = document.createElement('img')
-//       img.width = 150
-//       img.height = 150
-// document.getElementById('root').appendChild(img)
-
-
-// const moviesData = []
-//
-// for (let index = 550; index <= 650; index++) {
-//     const fetchData = async () => {
-//         const data = await fetch(`https://api.themoviedb.org/3/movie/${index}?api_key=46077ce24d4f82dbcaa2828a33a34916`)
-//         return await data.json()
-//     }
-//
-//
-//     const beginPath = 'https://image.tmdb.org/t/p/w500/'
-//     let imagePath = ''
-//
-//     fetchData()
-//         .then(response => {
-//             imagePath = beginPath + response.poster_path
-//             elementImg.src = imagePath
-//
-//             const movie = {
-//                 title: response.original_title,
-//                 release: response.release_date,
-//                 synopsis: response.overview,
-//                 poster: beginPath + response.poster_path,
-//                 genre: [
-//                     response.genres[0].name,
-//                     response.genres[1].name
-//                 ],
-//             }
-//             moviesData.push(movie)
-//         })
-//         .then(() => console.log(moviesData[0].title))
-//         .catch(err => err)
-//
-//     const elementImg = document.createElement('img')
-//           elementImg.width = 150
-//           elementImg.height = 150
-//
-//
-//     document.getElementById('root').appendChild(elementImg)
-// }
-//
-// console.log(moviesData)
-//
-// movies
-// Data.forEach(el => console.log(el))
-
 const saveData =  (array) => {
     title.push(array.original_title)
     releaseDate.push(array.release_date)
@@ -87,8 +20,13 @@ for (let index = 550; index <= 700; index++) {
     const root = document.getElementById('root')
           root.style.display = 'flex'
           root.style.flexWrap = 'wrap'
+
     const container = document.createElement('div')
           container.style.width = '200px'
+          container.style.height = '950px'
+          container.style.gap = '25px'
+          container.style.border = '1px solid black'
+
     const cardTitle = document.createElement('h3')
     const cardReleaseDate = document.createElement('p')
     const cardOverview = document.createElement('p')
@@ -98,20 +36,22 @@ for (let index = 550; index <= 700; index++) {
 
     getData()
         .then(res => {
+            console.log(res)
             saveData(res)
-            cardTitle.innerText = res.original_title
-            cardReleaseDate.innerText = res.release_date
-            cardOverview.innerText = res.overview
-            cardImg.src = beginPath + res.poster_path
+            if (res.success !== false) {
+                cardTitle.innerText = res.original_title
+                cardReleaseDate.innerText = res.release_date
+                cardOverview.innerText = res.overview
+                cardImg.src = beginPath + res.poster_path
+
+                container.appendChild(cardImg)
+                container.appendChild(cardTitle)
+                container.appendChild(cardReleaseDate)
+                container.appendChild(cardOverview)
+                root.appendChild(container)
+            }
         })
         .catch(error => error)
-
-    container.appendChild(cardTitle)
-    container.appendChild(cardReleaseDate)
-    container.appendChild(cardOverview)
-    container.appendChild(cardImg)
-    root.appendChild(container)
-
 }
 
 console.log(title)
