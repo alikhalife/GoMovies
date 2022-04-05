b0d8938aa4440d49b963f20b2ede4ced8384451e
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-04-05 23:09:09 +0200

    update carrousel

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,19 +1,32 @@
-function nextSlide(){
-    let activeSlide = document.querySelector('.slide.translate-x-0');
-    activeSlide.classList.remove('translate-x-0');
-    activeSlide.classList.add('-translate-x-full');
-    
-    let nextSlide = activeSlide.nextElementSibling;
-    nextSlide.classList.remove('translate-x-full');
-    nextSlide.classList.add('translate-x-0');
-}
-
-function previousSlide(){
-    let activeSlide = document.querySelector('.slide.translate-x-0');
-    activeSlide.classList.remove('translate-x-0');
-    activeSlide.classList.add('translate-x-full');
-    
-    let previousSlide = activeSlide.previousElementSibling;
-    previousSlide.classList.remove('-translate-x-full');
-    previousSlide.classList.add('translate-x-0');
+let screenW = window.innerWidth;
+let screenH = window.innerHeight;
+let pomme = screenW + 'px';
+let counter = 1;
+console.log(pomme);
+document.body.onload=function(){
+    nbr=3;
+    p=0;
+    contain_carrousel=document.getElementById('contain_carrousel');
+    right=document.getElementById('r');
+    contain_carrousel.style.width=(screenW*nbr)+"px";
+    for (i = 1; i <= nbr;i++){
+        div=document.createElement("div");
+        div.className='photo';
+        div.style.width = pomme;
+        div.style.backgroundImage="url('/assets/images/Film"+i+".jpg')";
+        contain_carrousel.appendChild(div);
+    }
+    console.log(screenW);
+    right.onclick=function(){
+        if(counter === 3){
+            counter=0;
+            p=1;
+        }
+        p--;
+        counter++;
+        contain_carrousel.style.transform="translate("+p*screenW+"px)";
+        contain_carrousel.style.transition="all 0.5s ease";
+        console.log(counter);
+        // console.log(p);
+    }
 }
