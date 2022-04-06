89ada6e38b09cae95b0145824abc787b6a2679e6
Christopher Delecluse <christopherdeleclusepro@gmail.com>
2022-04-06 08:30:49 +0200

    rename some variables

--- assets/scripts/components/Card.js
+++ assets/scripts/components/Card.js
@@ -1,20 +1,19 @@
 class Card {
     constructor(movie, elementRoot) {
         this.elementRoot = elementRoot
-        this.beginPath = 'https://image.tmdb.org/t/p/w500'
-        this.image = movie.poster_path
+        this.sourceUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         this.title = movie.original_title
         this.overview= movie.overview
         this.releaseDate = movie.release_date
     }
 
-    createElementHtml() {
+    appendCardInHtml() {
         return this.elementRoot.innerHTML +=  ` 
             <div>
                 <h3>${this.title}</h3>
             </div>
             <div>
-                <img src="${this.beginPath + this.image}" alt="image of movie">
+                <img src="${this.sourceUrl}" alt="image of movie">
                 <div>
                     <p>${this.releaseDate}</p>
                     <p>${this.overview}</p>
@@ -23,7 +22,7 @@ class Card {
     }
 }
 
-let cardElement = document.getElementById('card')
+let cardElementRoot = document.getElementById('card')
 
 const fetchTrendingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/trending/all/day?api_key=46077ce24d4f82dbcaa2828a33a34916')
@@ -34,7 +33,8 @@ fetchTrendingMovies()
     .then(response => {
         console.log(response)
         response.results.forEach(el => {
-            const newCard = new Card(el, cardElement)
-            newCard.createElementHtml()
+            const card = new Card(el, cardElementRoot)
+            card.appendCardInHtml()
         })
     })
+    .catch(error => error)
