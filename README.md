59cb2a884433fb70b8ef594e9c0ee548a109efce
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-04-07 16:10:20 +0200

    updates burger

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GoMovies
+
+## Languages used
+
+![](/assets/images/languages/HTML.svg "HTML")
+![](/assets/images/languages/GitHub.svg "GitHub")
+![](/assets/images/languages/Git.svg "Git")
+![](/assets/images/languages "a title")
+![](/assets/images/languages/HTML.svg "a title")
+![](/assets/images/languages/HTML.svg "a title")
+![](/assets/images/languages/HTML.svg "a title")
+![](/assets/images/languages/HTML.svg "a title")
+
+### Colaborators
--- assets/scripts/burger.js
+++ assets/scripts/burger.js
@@ -1,16 +1,12 @@
 let burger = document.getElementById('burger');
-let links = document.getElementById('links');
-
-let boolean = false;
-
 burger.addEventListener('click', ()=>{
-    links.style.display = "block";
-    // links.setAttribute
-    boolean = true;
-    if (boolean = true) {
-        burger.addEventListener('click', () => {
-            links.style.display= 'none';
-            boolean = false;
-        });
-    };
+    let x = document.getElementById('link');
+
+    if (x.classList.contains('links')) {
+        x.classList.remove('links')
+        x.classList.add('links_active')
+    } else if (x.classList.contains('links_active')) {
+        x.classList.remove('links_active')
+        x.classList.add('links')
+    }
 });
