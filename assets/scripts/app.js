a8ea98f699e50f6c7a80b538f89115f40f120273
Julie <juptsr.becode@gmail.com>
2022-04-08 14:31:55 +0200

    trailer on modal (some pb left with the trailer ID)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,12 @@
 let i = 0
 const arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
 let arrObj = ['objVide']
+let movie_id = 550
+let j = 0
+let arrIdTrailer = ['idVide']
+let arrOfKeyTrailer = ['vide']
+
+let a = 0
 
 const arrayOfCard = []
 
@@ -20,25 +26,49 @@ const fetchTrendingMoviesGenres = async () => {
     return await data.json()
 }
 
+let fetchMoviesTrailers = async () => {
+    j++
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${arrIdTrailer[j]}/videos?${api_key}&language=en-US`)
+    return await data.json()
+}
+
 fetchTrendingMovies()    
     .then(fetchTrendingMovieResponse => {
         console.log(fetchTrendingMovieResponse)
         fetchTrendingMovieResponse.results.forEach(el => {
             arrayOfCard.push(new Card_Modal(el, cardElementRoot))
+
         })
         return fetchTrendingMoviesGenres()
     })
     .then(genreResponse => {
         arrayOfCard.forEach(el => {
+            movie_id = el.id
+            arrIdTrailer.push(movie_id)
+            
             genreResponse.genres.forEach(elem => {
                 if(el.genreID === elem.id){
                     el.genre = elem.name
                 }
             })
+            
+            fetchMoviesTrailers()
+                .then(trailer => {
+                    a++
+                    // console.log('fetchMoviesTrailers')
+                    // console.log(trailer.results[0].key)
+                    // el.trailerKey = trailer.results[0].key
+                    arrOfKeyTrailer.push(trailer.results[1].key)
+                    console.log('a  ' + a)
+                    console.log(trailer.results[1].key)
+                    // console.log(el.trailerKey)
+                })
+
             el.appendCardInHtml()
             el.createObj()
         })
         console.log(arrayOfCard)
+        console.log(el.createObj())
     })
     .catch(error => error)
 
@@ -47,6 +77,7 @@ window.addEventListener('click', (event) => {
     if(event.target.matches('img.affiche') || event.target.matches('h2.card-title') || event.target.matches('h4.card-directed') || event.target.matches('p.card-stars') || event.target.matches('span.card-rate') || event.target.matches('span.genres')){
         console.log('bon div')
         console.log(window.innerWidth)
+        console.log(arrOfKeyTrailer)
 
         if(window.innerWidth > 600){
             div_modal.style.display = 'flex'
@@ -55,10 +86,13 @@ window.addEventListener('click', (event) => {
                 if(el === event.target.getAttribute('id')){
                     console.log(el)
                     console.log(arrObj[el].title)
+                    console.log(arrObj[el])
+                    console.log(arrOfKeyTrailer[el])
     
                     trailer_infos.innerHTML = `
-                        <iframe width="659.4" height="371" src="${arrObj[el].trailer}" title="${arrObj[el].title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    
+                    
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/${arrOfKeyTrailer[el]}" title="${arrObj[el].title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        
                         <div class="movie-details">
                             <p>${arrObj[el].statusMovie}</p>
                             <p>${arrObj[el].date}</p>
--- assets/scripts/components/Card.js
+++ assets/scripts/components/Card.js
@@ -9,25 +9,11 @@ class Card_Modal {
         this.overview= movie.overview
         this.releaseDate = movie.release_date
         this.rate = movie.vote_average
+        this.trailerKey = ''
     }
 
     appendCardInHtml() {
         i++
-    
-        // return this.root.innerHTML +=  ` 
-        //     <div>
-        //         <h3>${this.title}</h3>
-        //     </div>
-        //     <div>
-        //         <p>${this.genre}</p>
-        //     </div>
-        //     <div>
-        //         <img id="${i}" class="affiche" src="${this.poster}" alt="image of movie">
-        //         <div>
-        //             <p>${this.releaseDate}</p>
-        //             <p>${this.overview}</p>
-        //         </div>
-        //     </div> `
 
         return this.root.innerHTML +=  ` 
         <div class="card">
@@ -69,7 +55,7 @@ class Card_Modal {
     createObj(){
         const obj = {
             id: i,
-            trailer: 'url',
+            trailer: this.trailerKey,
             movieStatus: 'Release',
             date: this.releaseDate,
             directedBy: 'Mme',
@@ -77,41 +63,9 @@ class Card_Modal {
             title: this.title,
             synopsis: this.overview
         }
+
         arrObj.push(obj)
+        
+        return arrObj
     }
 }
-
-// class ModalVideo{
-//     constructor(movie, vidHTML){
-//         this.vidHTML = vidHTML
-//         this.videoUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-//         this.name = movie.original_title
-//         this.status = 'Release'
-//         this.date = movie.release_date
-//         this.directedBy = 'Mme'
-//         this.distribution = '1, 2, 3'
-//         this.synopsis = movie.overview
-//     }
-
-//     appendModalVideoInfosinHtml(){
-//         return this.vidHTML.innerHTML += `        
-//             <video id="trailer-vid" class="trailer-video" controls>
-//                 <source src="${this.videoUrl}" type="video/mp4">
-//                 Your browser does not support HTML video.
-//             </video>
-  
-//             <div class="movie-details">
-//                 <p>${this.status}</p>
-//                 <p>${this.date}</p>
-//                 <p>${this.directedBy}</p>
-//                 <p>${this.distribution}</p>
-//             </div>`
-//     }
-
-//     appendModalNameSynopsisinHtml(){
-//         return this.vidHTML.innerHTML += `
-//         <h4 class="movie-name"><strong>${this.name}</strong></h4>
-  
-//         <p class="synopsis">${this.synopsis}</p>`
-//     }
-// }
